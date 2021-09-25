e9bed413b2fcd3e962fbabee02a79342c5664671
RIYA BHATT <85434084+riya2310@users.noreply.github.com>
2021-09-25 12:08:16 +0530

    Add files via upload

---
+++ fun_overloadding.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#define pi 3.14
+using namespace std;
+int area(int a);
+int area(int l,int b);
+double area(int r,double f);
+int main(){
+int a,l,b,r;
+cin>>a>>l>>b>>r;
+cout<<"area of square is:"<<area(a)<<endl;
+cout<<"area of rectangle is:"<<area(l,b)<<endl;
+cout<<"area of circle:"<<area(r,pi)<<endl;
+}
+int area (int a){
+int b=a*a;
+return b;
+}
+int area(int l,int b){
+int c=l*b;
+return c;
+}
+double area(int r,double f){
+double d=f*r*r;
+return d;
+}
+
+
